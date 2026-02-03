ac6b9917ea65be2d7242a0e0968006b44012942c
anowlen <anowlen@usc.edu>
2026-02-02 20:41:28 -0800

    adding actuator code

---
+++ MakersActuator/MakersActuator.ino
@@ -0,0 +1,54 @@
+String command = "";
+
+// Motor direction pins
+const int IN1 = 9;     
+const int IN2 = 10;
+const int ENA = 6;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+
+  // Start stopped
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  //analogWrite(ENA, 255);
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    command = Serial.readStringUntil('\n');
+    command.trim();
+
+    if (command == "up") {
+      moveForward();
+    }
+
+    if (command == "down") {
+      moveBackward();
+    }
+  }
+}
+
+void moveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  delay(250);
+  stopMotor();
+}
+
+void moveBackward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  delay(250);
+  stopMotor();
+}
+
+void stopMotor() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+}
